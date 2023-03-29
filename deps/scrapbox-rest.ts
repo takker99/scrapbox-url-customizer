b706073cfa246da7045a4acc42f16abec6c55a11
takker99 <37929109+takker99@users.noreply.github.com>
2023-03-29 16:16:38 +0900

    :+1: `GM_fetch`なしでもtweetを取得する

--- deps/scrapbox-rest.ts
+++ deps/scrapbox-rest.ts
@@ -1,1 +1,2 @@
 export * from "https://raw.githubusercontent.com/takker99/scrapbox-userscript-std/0.20.0/rest/mod.ts";
+export * from "https://raw.githubusercontent.com/scrapbox-jp/types/0.4.2/rest.ts";
