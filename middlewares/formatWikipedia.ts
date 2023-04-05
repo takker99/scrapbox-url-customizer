b5c61e0da9c329a79163baa8f66bf7c618502ebe
takker99 <37929109+takker99@users.noreply.github.com>
2023-04-06 08:59:22 +0900

    :+1: wikipediaのmobile版URLに対応した

--- middlewares/formatWikipedia.ts
+++ middlewares/formatWikipedia.ts
@@ -1,7 +1,13 @@
 /** wikipediaのリンクを変換する */
 export const formatWikipedia = (url: URL): string | URL => {
-  if (!/^\w+\.wikipedia\.org$/.test(url.hostname)) return url;
   if (!url.pathname.startsWith("/wiki/")) return url;
+  if (!/^\w+\.wikipedia\.org$/.test(url.hostname)) {
+    const [, lang] = url.hostname.match(/^(\w+)\.m\.wikipedia\.org$/) ?? [];
+    if (!lang) {
+      return url;
+    }
+    url.hostname = `${lang}.wikipedia.org`;
+  }
 
   const title = decodeURIComponent(url.pathname.slice(6));
   const section = url.hash ? decodeURIComponent(url.hash.slice(1)) : "";
