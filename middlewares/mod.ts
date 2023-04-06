72e794dbf080a556f5c3ccda6d0a38b8ba698a95
takker99 <37929109+takker99@users.noreply.github.com>
2023-04-06 09:03:11 +0900

    :sparkles: Google検索結果URLを展開する

--- middlewares/mod.ts
+++ middlewares/mod.ts
@@ -2,5 +2,6 @@ export * from "./formatTweet.ts";
 export * from "./expandShortURL.ts";
 export * from "./formatWikipedia.ts";
 export * from "./redirectWikiwand.ts";
+export * from "./redirectGoogleSearch.ts";
 export * from "./formatURL.ts";
 export * from "./convertScrapboxURL.ts";
