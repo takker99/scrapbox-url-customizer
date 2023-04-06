1883cd57a385fe029e88b7dd8d25c55b701dd0fe
takker99 <37929109+takker99@users.noreply.github.com>
2023-04-06 11:13:33 +0900

    :bug: 正規表現ミス

--- middlewares/shortenAmazonURL.ts
+++ middlewares/shortenAmazonURL.ts
@@ -6,7 +6,7 @@
  */
 export const shortenAmazonURL = (url: URL): URL => {
   if (
-    !/^(?:www\.)?amazon(?\.co|com)?\.(?:au|br|ca|fr|de|in|it|jp|mx|nl|sg|es|tr|ae|uk|cn)$/
+    !/^(?:www\.)?amazon(?:\.co|com)?\.(?:au|br|ca|fr|de|in|it|jp|mx|nl|sg|es|tr|ae|uk|cn)$/
       .test(url.hostname)
   ) return url;
   const [, asin] = url.pathname.match(/\/dp\/([\w\d]+)/) ??
